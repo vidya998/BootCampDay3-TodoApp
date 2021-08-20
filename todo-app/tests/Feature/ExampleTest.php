664ff3e8c1ca6409e76fb23a23c2a171d7f2b4e6
vidya998 <vidya.verma@razorpay.com>
2021-08-20 20:39:13 +0530

    Reviewed Code

--- todo-app/tests/Feature/ExampleTest.php
+++ todo-app/tests/Feature/ExampleTest.php
@@ -18,4 +18,27 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_taskInsertionTest()
+    {
+        $response = $this->json('POST', '/api/task', ["id" => 11, "task_name"=> "Shopping",
+            "task_description"=> "Buy egg,milk", "task_status"=> "pending"]);
+        $response -> assertStatus(200);
+    }
+
+    public function test_taskDeletionTest()
+    {
+        $response = $this->json('DELETE', '/api/task/del', ["id" => 1]);
+        $response -> assertStatus(200);
+    }
+    public function test_taskUpdationTest()
+    {
+        $response = $this->json('PUT', '/api/task/', ["id" => 2, "task_status"=> "progress"]);
+        $response -> assertStatus(200);
+    }
+    public function test_taskFetchTest()
+    {
+        $response = $this->json('GET', '/api/task/fetch', ["id" => 2]);
+        $response -> assertStatus(200);
+    }
 }
